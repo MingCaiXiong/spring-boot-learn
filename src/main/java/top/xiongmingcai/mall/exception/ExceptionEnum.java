1b7ccbd28320d74769b7a5a7ed767d28561cf9e0
xmc <xmc000@icloud.com>
2021-04-28 18:16:51 +0800

    fix style

--- src/main/java/top/xiongmingcai/mall/exception/ExceptionEnum.java
+++ src/main/java/top/xiongmingcai/mall/exception/ExceptionEnum.java
@@ -4,7 +4,12 @@ package top.xiongmingcai.mall.exception;
  * 异常枚举
  */
 public enum ExceptionEnum {
-    NEED_USER_NAME(10001, "用户名不能为空"), NEED__LESS_THAN_8_BITS(10002, "密码不能少于8位"), NEED_PASSWORD(10003, "密码不能为空"), NEED_DO_NOT_ALLOW_DUPLICATE_NAMES(10004, "不允许重名, 注册失败"), USER_CREATION_FAILED(10005, "用户入库失败");
+    NEED_USER_NAME(10001, "用户名不能为空"),
+    NEED__LESS_THAN_8_BITS(10002, "密码不能少于8位"),
+    NEED_PASSWORD(10003, "密码不能为空"),
+    NEED_DO_NOT_ALLOW_DUPLICATE_NAMES(10004,
+            "不允许重名, 注册失败"),
+    USER_CREATION_FAILED(10005, "用户入库失败");
     private Integer code;
     private String msg;
 
