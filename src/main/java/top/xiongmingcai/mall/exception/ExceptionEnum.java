9c0f52da3b2f4aa62debd9571e787600a75c3ace
xmc <xmc000@icloud.com>
2021-04-28 15:51:52 +0800

    4-3 API统一返回对象-慕课网体系课

---
+++ src/main/java/top/xiongmingcai/mall/exception/ExceptionEnum.java
@@ -0,0 +1,39 @@
+package top.xiongmingcai.mall.exception;
+
+/**
+ * 异常枚举
+ */
+public enum ExceptionEnum {
+    NEED_USER_NAME(10001, "用户名不能为空");
+    private Integer code;
+    private String msg;
+
+    ExceptionEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "ExceptionEnum{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
